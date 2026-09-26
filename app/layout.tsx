4ff8306ffdba408c23d9c1792c9800fb48733ea1
Niko <naara21@hotmail.de>
2026-09-26 11:26:58 +0200

    feat(toast): add ToastProvider and useToast hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastProvider } from "./components/toast/ToastProvider";
 import { auth } from "@/lib/auth/server";
 import { canCreateRecipes } from "@/dbQueries";
 
@@ -40,13 +41,17 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
           some browsers can otherwise introduce a page-wide horizontal
           scrollbar. */}
       <body className="flex min-h-full flex-col overflow-x-hidden">
-        <Header user={user} canCreateRecipes={canCreate} />
+        {/* A Client Component provider can wrap Server Components: they're
+            passed in as already-rendered `children`. */}
+        <ToastProvider>
+          <Header user={user} canCreateRecipes={canCreate} />
 
-        <div className="relative flex flex-1 flex-col">
-          <main className="flex-1">{children}</main>
+          <div className="relative flex flex-1 flex-col">
+            <main className="flex-1">{children}</main>
 
-          <Footer user={user} canCreateRecipes={canCreate} />
-        </div>
+            <Footer user={user} canCreateRecipes={canCreate} />
+          </div>
+        </ToastProvider>
       </body>
     </html>
   );
